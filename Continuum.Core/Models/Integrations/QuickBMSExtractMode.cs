ae7587652620c7ec3a202c3f35e71e5ae8233d07
Dasorik <danielrsteven@gmail.com>
2022-03-06 07:48:02 +0000

    Added QuickBMSExtractMode property to game integrations

---
+++ Continuum.Core/Models/Integrations/QuickBMSExtractMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Continuum.Core.Models
+{
+	public enum QuickBMSExtractMode
+	{
+		NamedFolder,
+		RootFolder,
+		StaticFolder
+	}
+}
